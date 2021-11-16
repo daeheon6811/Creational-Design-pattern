512201d709fefe97010ee7f0c47c3edc637f5ca8
PDH <54939319+wow1186@users.noreply.github.com>
2021-11-16 12:10:20 +0900

    #daeheon [Add] python factory 추가

---
+++ FactoryPattern/java/Cat.java
@@ -0,0 +1,5 @@
+package FactoryPattern.java;
+
+public class Cat{
+    
+}
---
+++ FactoryPattern/python/python-factory.py
@@ -0,0 +1,44 @@
+from enum import Enum
+class AnimalEnum(Enum):
+  CAT = 1
+  DOG = 2
+
+class Animal():
+  def speak(self):
+    pass
+
+class Cat(Animal):
+  def speak(self):
+    print("meow")
+
+class Dog(Animal):
+  def speak(self):
+    print("bark")
+    
+#Factory function
+#prefer enum
+def FactoryFn(animal:AnimalEnum):
+  if animal == AnimalEnum.CAT:
+    return Cat()
+  elif animal == AnimalEnum.DOG:
+    return Dog()
+
+
+cat = FactoryFn(AnimalEnum.CAT)
+cat.speak()
+dog = FactoryFn(AnimalEnum.DOG)
+dog.speak()
+
+#Factory class
+class AnimalFactory():
+  def createAnimal(self,animal):
+    if animal == AnimalEnum.CAT:
+      return Cat()
+    elif animal == AnimalEnum.DOG:
+      return Dog()
+
+animal_factory = AnimalFactory()
+cat = animal_factory.createAnimal(AnimalEnum.CAT)
+cat.speak()
+dog = animal_factory.createAnimal(AnimalEnum.DOG)
+dog.speak()
